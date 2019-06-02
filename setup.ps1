397355986dbe768db65619f7c3926682cd795e1b
minhlai <minh.lai30@gmail.com>
2019-06-01 22:22:11 -0700

    comment out ngen to make optional

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,6 @@
-# this will greatly decreased startup time for powershell
-Set-Alias ngen (Join-Path ([Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()) ngen.exe)
-ngen update
+# if powershell is too slow, ngen can greatly decreased startup time for powershell. requires admin rights
+# Set-Alias ngen (Join-Path ([Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()) ngen.exe)
+# ngen update
 
 # make sure virtual environment is setup for python
 pip install virtualenv
