64e3be76e9ba135d000467f0db06d957242ba73a
minhlai <minh.lai30@gmail.com>
2019-06-01 22:20:29 -0700

    ngen update to setup

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,13 @@
+# this will greatly decreased startup time for powershell
+Set-Alias ngen (Join-Path ([Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()) ngen.exe)
+ngen update
+
+# make sure virtual environment is setup for python
 pip install virtualenv
 py -m venv myvenv
 .\myvenv\Scripts\activate
 python -m pip install --upgrade pip
 pip install -r .\requirements.txt
+
+# run server
 py .\django\manage.py runserver 0.0.0.0:80
